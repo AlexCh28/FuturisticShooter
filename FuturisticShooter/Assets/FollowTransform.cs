c1d4237cfd445c1c68c3bc9ecd322e46b19d4447
Alexander <sania.chaschin2011@yandex.ru>
2023-04-04 16:02:10 +0300

    Добавил риги для рук
    Руки прикреплены к таргетам оружия, но ладони пока не настроены.

--- FuturisticShooter/Assets/FollowTransform.cs
+++ FuturisticShooter/Assets/FollowTransform.cs
@@ -8,9 +8,16 @@ public class FollowTransform : MonoBehaviour
     private Transform _targetTransform;
 
     [SerializeField]
+    private Transform _parentTransform;
+
+    [SerializeField]
     private Vector3 _offset = Vector3.zero;
 
+    private void Awake() {
+        if (!_parentTransform) _parentTransform = _targetTransform;
+    }
+
     private void Update() {
-        transform.position = _targetTransform.TransformPoint(_targetTransform.localPosition + _offset);
+        transform.position = _parentTransform.TransformPoint(_targetTransform.localPosition + _offset);
     }
 }
